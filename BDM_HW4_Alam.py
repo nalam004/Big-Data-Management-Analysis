956b5dcaa236bcf059571b69413d07e12f90ce43
Niharika Alam <70300853+nalam004@users.noreply.github.com>
2021-12-05 16:01:19 -0500

    Update BDM_HW4_Alam.py

--- BDM_HW4_Alam.py
+++ BDM_HW4_Alam.py
@@ -3,8 +3,11 @@ import sys
 
 if __name__=='__main__':
     sc = SparkContext()
-    sc.textFile(sys.argv[1] if len(sys.argv)>1 else 'book.txt') \
-        .flatMap(lambda x: x.split()) \
-        .map(lambda x: (x,1)) \
-        .reduceByKey(lambda x,y: x+y) \
-        .saveAsTextFile(sys.argv[2] if len(sys.argv)>2 else 'output')
+    
+    NAICS = set(['452210', '445120', '722410', '722511', '722513', '446110', '446191', '311811', '722515', '445210', '445220', '445230', '445291', '445292', '445299', '445110'])
+
+    
+    core_places = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in NAICS).cache()
+    weekly_pattern = sc.textFile('weekly-patterns-nyc-2019-2020').filter(lambda x: next(csv.reader([x]))[1] in core_places).cache()
+    
+    #.saveAsTextFile('output')
