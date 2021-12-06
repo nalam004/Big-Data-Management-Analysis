ce7c860709c7b5d1927fb52fce2e8c6d02180656
Niharika Alam <70300853+nalam004@users.noreply.github.com>
2021-12-05 23:55:11 -0500

    Update BDM_HW4_Alam.py

--- BDM_HW4_Alam.py
+++ BDM_HW4_Alam.py
@@ -1,52 +1,67 @@
-from pyspark import SparkContext
-import sys
+import csv
+import datetime
+import json
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import DateType, IntegerType, MapType, StringType
 from datetime import datetime as dt, timedelta as td
 import ast
+import pyspark.sql.functions as func 
 
 if __name__=='__main__':
-    sc = SparkContext()
+    sc = pyspark.SparkContext()
+    spark = SparkSession(sc)
     
-    def getVisits(x):
-      dic = {}
-      visits = []
-      lis = ast.literal_eval(x[3])
-      start = x[1].split('T')[0]
-      end = x[2].split('T')[0]
+    # returns number of visits for the day
+    def expandVisits(date_range_start, visits_by_day):
+        dic = {}
+        lis = ast.literal_eval(visits_by_day)
+        start = date_range_start.split('T')[0]
+        sd = dt.strptime(start,'%Y-%m-%d')
+        end = sd + datetime.timedelta(days=len(lis))
 
-      sd = dt.strptime(start,'%Y-%m-%d')
-      ed = dt.strptime(end,'%Y-%m-%d')
-      filtered_date = dt.strptime('2020-03-17','%Y-%m-%d')
-      delta = ed - sd
+        delta = end - sd
+        for i in range(delta.days):
+            dic[(sd + td(days = i)).date()] = lis[i]
+            
+        return (dic)
+    
+    # code for each store type
+    NAICS = set(['452210', '452311', '445120', '722410', '722511', '722513', '446110', '446191', '311811', '722515', '445210', '445220', '445230', '445291', '445292', '445299', '445110']) 
 
-      for i in range(delta.days):
-        dic[sd + td(days = i)] = lis[i]
+    # file for only items with one of the 9 store type
+    core_places = spark.read.csv('core-places-nyc.csv', header=True, escape='"').where(F.col('naics_code').isin(NAICS))
+    weekly_patterns = spark.read.csv('weekly-patterns-nyc-2019-2020', header=True, escape='"')
+    
+    big_box_grocers = core_places.where(F.col('naics_code').isin(set(['452210'])))
+    convenience_stores = core_places.where(F.col('naics_code').isin(set(['445120']))) 
+    drinking_places = core_places.where(F.col('naics_code').isin(set(['722410']))) 
+    full_service_restaurants = core_places.where(F.col('naics_code').isin(set(['722511']))) 
+    limited_service_restaurants = core_places.where(F.col('naics_code').isin(set(['722513']))) 
+    pharmacies_drug_stores = core_places.where(F.col('naics_code').isin(set(['446110', '446191'])))
+    snack_bakeries = core_places.where(F.col('naics_code').isin(set(['311811', '722515'])))
+    specialty_food_stores = core_places.where(F.col('naics_code').isin(set(['445210', '445220', '445230', '445291', '445292', '445299'])))
+    supermarkets = core_places.where(F.col('naics_code').isin(set(['445110'])))
 
-      for key, value in dic.items():
-        if (key >= filtered_date):
-          visits.append(value)
+    # joins the core_places with weekly_patterns data
+    joined = core_places.join(weekly_patterns, core_places.safegraph_place_id == weekly_patterns.safegraph_place_id, "inner")
+    
+    # shows only code, date_start, and visits by day
+    df = joined.select(core_places.safegraph_place_id, 'naics_code', 'date_range_start', 'visits_by_day')
+    
+    # maps data to expandVisits function
+    udfExpand = F.udf(expandVisits, MapType(DateType(), IntegerType()))
+    dfB = df.select('naics_code', F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'))
+    
+    # find median and high
+    median = dfB.groupBy('naics_code', 'date').agg(func.percentile_approx('visits', 0.5).alias("median"))
+    high = dfB.groupBy('naics_code', 'date').agg(func.max('visits').alias('high'))
 
-      return (x[0], visits)
-    
-    big_box_grocers = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['452210'])).cache()
-    convenience_stores = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['445120'])).cache() 
-    drinking_places = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['722410'])).cache() 
-    full_service_restaurants = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['722511'])).cache() 
-    limited_service_restaurants = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['722513'])).cache() 
-    pharmacies_drug_stores = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['446110', '446191'])).cache()
-    snack_bakeries = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['311811', '722515'])).cache()
-    specialty_food_stores = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['445210', '445220', '445230', '445291', '445292', '445299'])).cache() 
-    supermarkets = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in set(['445110'])).cache()
-    
-    NAICS = set(['452210', '445120', '722410', '722511', '722513', '446110', '446191', '311811', '722515', '445210', '445220', '445230', '445291', '445292', '445299', '445110']) 
-    
-    core_places = sc.textFile('core-places-nyc.csv').filter(lambda x: next(csv.reader([x]))[9] in NAICS).cache()
-    # filter by safegraph_place_id
-    weekly_pattern = sc.textFile('weekly-patterns-nyc-2019-2020').filter(lambda x: next(csv.reader([x]))[1] in core_places[1]).cache()
-    
-    # safegraph_place_id, "date_range_start", "date_range_end", "visits_by_day", "median_dwell"
-    rddA = weekly_pattern.map(lambda x: next(csv.reader([x]))).map(lambda x: (x[1], x[12], x[13], x[16], x[23]))
-    
-    
+    # joins median and high data
+    dfC = median.join(high, median.naics_code == high.naics_code)
+    dfD = dfC.select(median.date, 'median', 'high')
+ 
     
     
     #.saveAsTextFile('output')
